5eeb385c6c8f4d512e0b9f463344bbb25e587249
dennereed <denne.reed@gmail.com>
2020-07-10 13:07:09 -0700

    psr init

--- psr/admin.py
+++ psr/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Biology)
+admin.site.register(Archaeology)
+admin.site.register(Geology)
+admin.site.register(Locality)
+admin.site.register(Occurrence)
+admin.site.register(Taxon)
+admin.site.register(TaxonRank)
