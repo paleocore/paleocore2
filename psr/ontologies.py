6bc46dfd410fc861e4b0ea3b279edc05de2008ca
dennereed <denne.reed@gmail.com>
2021-01-10 22:41:49 -0600

    updates to psr

---
+++ psr/ontologies.py
@@ -0,0 +1,55 @@
+import projects.ontologies as ontologies
+
+# Basis of Record Vocabulary
+BASIS_OF_RECORD_VOCABULARY = ontologies.BASIS_OF_RECORD_VOCABULARY
+
+# Item Type Vocabulary
+ITEM_TYPE_VOCABULARY = ontologies.ITEM_TYPE_VOCABULARY
+
+# Collection Method Vocabulary
+COLLECTING_METHOD_VOCABULARY = ontologies.COLLECTING_METHOD_VOCABULARY
+
+# Anatomical Side Vocabulary
+SIDE_VOCABULARY = ontologies.SIDE_VOCABULARY
+
+# Collector Vocabulary
+zeresenay_alemseged = 'Zeresenay Alemseged'
+denis_geraads = 'Denis Geraads'
+yared_asseffa = 'Yared Asseffa'
+andrew_barr = 'Andrew Barr'
+rene_bobe = "Rene Bobe"
+tomas_getachew = "Tomas Getachew"
+weldeyared_hailu = "Waldeyared Hailu"
+shannon_mcpherron = "Shannon McPherron"
+denne_reed = "Denne Reed"
+peter_stamos = "Peter Stamos"
+jonathan_wynn = "Jonathan Wynn"
+COLLECTOR_CHOICES = (
+    (zeresenay_alemseged, "Zeresenay Alemseged"),
+    (yared_asseffa, "Yared Assefa"),
+    (andrew_barr, "Andrew Barr"),
+    (rene_bobe, "Rene Bobe"),
+    (denis_geraads, "Denis Geraads"),
+    (tomas_getachew, "Tomas Getachew"),
+    (weldeyared_hailu, "Waldeyared Hailu"),
+    (shannon_mcpherron, "Shannon McPherron"),
+    (denne_reed, "Denne Reed"),
+    (peter_stamos, "Peter Stamos"),
+    (jonathan_wynn, "Jonathan Wynn")
+)
+
+# Field Season Vocabulary
+jan2014 = 'January 2014'
+nov2014 = 'Nov 2014'
+nov2015 = 'Nov 2015'
+jan2018 = 'Jan 2018'
+jan2019 = 'Jan 2019'
+jan2020 = 'Jan 2020'
+FIELD_SEASON_CHOICES = (
+    (jan2014, 'Jan 2014'),
+    (nov2014, 'Nov 2014'),
+    (nov2015, 'Nov 2015'),
+    (jan2018, 'Jan 2018'),
+    (jan2019, 'Jan 2019'),
+    (jan2020, 'Jan 2020')
+)
